ce532bb39f918d7a3bdb9b86ec5c5109f92612ae
Fischa7 <janosch.fischer89@googlemail.com>
2018-04-11 14:26:40 +0200

    implemented the most basic functionality of minting a number

--- contracts/CryptoNumbers.sol
+++ contracts/CryptoNumbers.sol
@@ -7,6 +7,17 @@ import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 contract CryptoNumbers is Ownable, ERC721Token("CryptoNumbers","CN") {
     
     using SafeMath for uint256;
+    // initial price for minting a new number
+    uint256 internal price = 1 finney;
+    // for later implementation of auctioning the numbers
+    uint256 public constant NO_STARTING_PRICE = 1 finney;
 
-
+    function setPrice(uint256 _price) external onlyOwner() {
+        price = _price;
+    }
+    
+    function mint(uint256 numId) public payable {
+        require(msg.value >= price);
+        _mint(msg.sender, numId);
+    }
 }
