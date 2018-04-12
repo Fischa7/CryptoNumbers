22b06a4f9975d6cd79425352ad7c868d133fdcbc
Fischa7 <janosch.fischer89@googlemail.com>
2018-04-12 18:58:01 +0200

    changed minting functionality, added function to get current number

--- contracts/CryptoNumbers.sol
+++ contracts/CryptoNumbers.sol
@@ -7,24 +7,42 @@ import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 contract CryptoNumbers is Ownable, ERC721Token("CryptoNumbers","CN") {
     
     using SafeMath for uint256;
+    
     // initial price for minting a new number
     uint256 internal price = 1 finney;
+
     // for later implementation of auctioning the numbers
     uint256 public constant NO_STARTING_PRICE = 1 finney;
+
+    // number that is currently being sold. starts at 0
+    uint256 internal currentNumber_ = 0;
 
     function setPrice(uint256 _price) external onlyOwner() {
         price = _price;
     }
     
     /// mints a new number. Each number can only be minted once.
-    function mint(uint256 numId) public payable {
+    function mint() public payable {
         require(msg.value >= price);
+        uint256 numId = getCurrentNumber();
         _mint(msg.sender, numId);
+        currentNumber_ = currentNumber_.add(1);
 
         // in case the user send to much money, send the rest of it back
         if (msg.value > price) {
             uint256 excess = msg.value.sub(price);
             msg.sender.transfer(excess);
         }
+    }
+
+    function showCurrentNumber() public view returns(uint256) {
+        return currentNumber_;
+    }
+
+    function getCurrentNumber() public returns (uint256) {
+        for (uint256 i = 0; i < 10; i.add(1)) {
+            currentNumber_ = currentNumber_.add(1);
+        }
+        return currentNumber_;
     }
 }
