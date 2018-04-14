6cccc85c88cb15c5a893748f6ff521026e7c8bab
Fischa7 <janosch.fischer89@googlemail.com>
2018-04-14 05:12:35 +0200

    changed variable namings, added first draft of number struct

--- contracts/CryptoNumbers.sol
+++ contracts/CryptoNumbers.sol
@@ -7,6 +7,11 @@ import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 contract CryptoNumbers is Ownable, ERC721Token("CryptoNumbers","CN") {
     
     using SafeMath for uint256;
+
+    struct Number {
+        uint64 birthtime;
+        uint64 cooldownEndblock;
+    }
     
     // initial price for minting a new number
     uint256 internal price = 1 finney;
@@ -15,34 +20,36 @@ contract CryptoNumbers is Ownable, ERC721Token("CryptoNumbers","CN") {
     uint256 public constant NO_STARTING_PRICE = 1 finney;
 
     // number that is currently being sold. starts at 0
-    uint256 internal currentNumber_ = 0;
+    uint256 internal nextToBeMinted_ = 0;
 
     function setPrice(uint256 _price) external onlyOwner() {
         price = _price;
     }
     
-    /// mints a new number. Each number can only be minted once.
+    /// mints the next available number. Each number can only be minted once.
     function mint() public payable {
         require(msg.value >= price);
-        uint256 numId = getCurrentNumber();
+        uint256 numId = getNextToBeMinted();
         _mint(msg.sender, numId);
-        currentNumber_ = currentNumber_.add(1);
+        nextToBeMinted_ = nextToBeMinted_.add(1);
 
-        // in case the user send to much money, send the rest of it back
+        // in case the user sent to much money, send the rest of it back
         if (msg.value > price) {
             uint256 excess = msg.value.sub(price);
             msg.sender.transfer(excess);
         }
     }
 
-    function showCurrentNumber() public view returns(uint256) {
-        return currentNumber_;
+    // displays the next number to be minted. 
+    function showNextToBeMinted() public view returns(uint256) {
+        return nextToBeMinted_;
     }
 
-    function getCurrentNumber() public returns (uint256) {
+    // gets the next number to be minted. 
+    function getNextToBeMinted() public returns (uint256) {
         for (uint256 i = 0; i < 10; i.add(1)) {
-            currentNumber_ = currentNumber_.add(1);
+            nextToBeMinted_= nextToBeMinted_.add(1);
         }
-        return currentNumber_;
+        return nextToBeMinted_;
     }
 }
